73a843b867188fe61323e0024ea8a8eb494e6b0e
riyabulia12 <riyabulia12@gmail.com>
2022-12-05 19:43:40 +0530

    Remove unused code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get "/users" to: "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
