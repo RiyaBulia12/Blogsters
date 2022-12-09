e5104447a8eb0b494d7032da1dc9f5151fcc976c
riyabulia12 <riyabulia12@gmail.com>
2022-12-09 21:36:48 +0530

    Add new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
   def show
@@ -10,4 +10,16 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_post_url(@post.user_id, @post.id)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
+  end
+
+
 end
