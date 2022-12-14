a056b47084d0dc293d98b13e20063740cba0a208
riyabulia12 <riyabulia12@gmail.com>
2022-12-15 00:25:41 +0530

    Fixed linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,16 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-      return unless user.present?
-      can :destroy, Comment, author_id: user.id
-      can :destroy, Post, author_id: user.id
+    return unless user.present?
 
-      # Allow all for admins
-      can :manage, :all if user.role? == "admin"
+    can :destroy, Comment, author_id: user.id
+    can :destroy, Post, author_id: user.id
+
+    # Allow all for admins
+    can :manage, :all if user.role? == 'admin'
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
