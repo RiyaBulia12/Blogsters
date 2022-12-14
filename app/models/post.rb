67a891c7a29eb24b2c1e5651a87264224de02183
riyabulia12 <riyabulia12@gmail.com>
2022-12-15 00:24:29 +0530

    Update comment & post counter inn models after destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
   after_save :update_posts_count
+  after_destroy :update_posts_count
+
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
