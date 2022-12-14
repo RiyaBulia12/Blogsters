663404eaf1369df189925f64d56132f91283d95a
riyabulia12 <riyabulia12@gmail.com>
2022-12-14 16:39:50 +0530

    Add sign out route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
-  get '/users', to: 'users#index'
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
+
+  root 'users#index'
+  get '/users', to: 'users#index'
 end
